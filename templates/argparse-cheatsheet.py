24faefafdac58d95b96ece79cc5a5fbd565917a4
Hauke D <haukex@zero-g.net>
2024-03-29 18:44:31 +0000

    A few updates making argparse usage more modular

--- templates/argparse-cheatsheet.py
+++ templates/argparse-cheatsheet.py
@@ -2,8 +2,9 @@
 import argparse
 from igbpyutils.file import autoglob, cmdline_rglob
 
-def main():
-    parser = argparse.ArgumentParser(description='Template')
+def _arg_parser():
+    # NOTE the first 'prog' argument here vvvvv is optional and defaults to sys.argv[0]
+    parser = argparse.ArgumentParser('argparse-cheatsheet', description='Template')
     parser.add_argument('-v', '--verbose', help="more output", action="store_true")
 
     # the following defaults `foo` to `None` and doesn't generate a short equivalent to `--no-foo` (e.g. `-X`):
@@ -16,7 +17,7 @@ def main():
     parser.add_argument('-Z', '--no-quz', help="Quz Off", dest="quz", action="store_false", default=None)
     parser.add_argument('--ignore-quz', help="No Quz", dest="quz", action="store_const", const=None)
 
-    subparsers = parser.add_subparsers(dest='cmd', required=True)
+    subparsers = parser.add_subparsers(dest='cmd', required=True, help="CMD -h for help")
 
     parser_demo = subparsers.add_parser('demo', help='Demo')
     parser_demo.add_argument('-a', '--add', help="add stuff to list", action="append", default=[])
@@ -25,7 +26,12 @@ def main():
     parser_demo.add_argument('something', choices=['foo', 'bar', 'quz'])  # required by default
     parser_demo.add_argument('paths', metavar='PATH', help="files/dirs to process (recursive)", nargs="*")
 
-    args = parser.parse_args()
+    return parser
+
+def main(argv=None):
+    parser = _arg_parser()  # feel free to inline (unless the object is needed elsewhere, like for docs)
+
+    args = parser.parse_args(argv)  # argv=None means sys.argv[1:]
     if args.choice != 'one':
         parser.error("In this demo you must choose 'one'")
 
