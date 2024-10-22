94990d1ce4ea60efea37c285e0c05d54d24540d4
Hauke D <haukex@zero-g.net>
2024-10-22 20:28:36 +0200

    Updated vscode user settings backup

--- bak-vscode-user-sett.py
+++ bak-vscode-user-sett.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 from pathlib import Path
 import subprocess
+import sys
 import re
 
-sett_json = Path.home()/'AppData'/'Roaming'/'Code'/'User'/'settings.json'
-bak_file = Path(__file__).parent/'notes'/'vscode-user-settings-win.bak.jsonc'
+if sys.platform.startswith('win32'):
+    sett_json = Path.home()/'AppData'/'Roaming'/'Code'/'User'/'settings.json'
+elif sys.platform.startswith('linux'):
+    sett_json = Path.home()/'.config'/'Code'/'User'/'settings.json'
+else:
+    raise NotImplementedError()
+bak_file = Path(__file__).parent/'notes'/'vscode-user-settings.bak.jsonc'
 
 with sett_json.open(encoding='UTF-8') as fh:
     json = fh.read()
@@ -14,7 +20,7 @@ json = re.sub(r'(^\s*"remote.SSH.remotePlatform":\s+\{)[^}]+(\},\n)',
               lambda m: m.group(1)+' /* (content removed in backup) */ '+m.group(2), json, flags=re.S|re.M) \
 
 with bak_file.open('w', encoding='UTF-8', newline='\n') as fh:
-    fh.write(f"// This is a backup of my ~/{sett_json.relative_to(Path.home()).as_posix()}\n")
+    fh.write(f"// This is a backup of my VSCode user settings.json\n")
     fh.write(json)
 
 subprocess.run(['git','diff','--color-words','--ignore-all-space',sett_json,bak_file], check=False)
