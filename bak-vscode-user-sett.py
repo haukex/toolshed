4d5dd30903a621d5854e49c0cd0fd82f737c5e4f
Hauke D <haukex@zero-g.net>
2026-08-21 17:29:27 +0200

    Updated VSCode settings.json

--- bak-vscode-user-sett.py
+++ bak-vscode-user-sett.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+# pylint: disable=invalid-name
 from pathlib import Path
 import subprocess
 import sys
 import re
+import os
+
+# spell: ignore LOCALAPPDATA
 
 if sys.platform.startswith('win32'):
     sett_json = Path.home()/'AppData'/'Roaming'/'Code'/'User'/'settings.json'
@@ -17,10 +21,15 @@ with sett_json.open(encoding='UTF-8') as fh:
 
 # replacements are kind of ugly, but I haven't yet found a well-supported JSON5/JSONC module that preserves comments
 json = re.sub(r'(^\s*"remote.SSH.remotePlatform":\s+\{)[^}]+(\},\n)',
-              lambda m: m.group(1)+' /* (content removed in backup) */ '+m.group(2), json, flags=re.S|re.M) \
+              lambda m: m.group(1)+' /* (content removed in backup) */ '+m.group(2), json, flags=re.S|re.M)
+
+appdata = os.getenv('LOCALAPPDATA')
+assert appdata is not None
+json = json.replace('"'+appdata.replace('\\','\\\\'), '/* NOTICE: Expand LOCALAPPDATA manually! */ "%LOCALAPPDATA%')
 
 with bak_file.open('w', encoding='UTF-8', newline='\n') as fh:
     fh.write("// This is a backup of my VSCode user settings.json\n")
     fh.write(json)
 
 subprocess.run(['git','diff','--color-words','--ignore-all-space',sett_json,bak_file], check=False)
+subprocess.run(['git','diff','--color-words'], check=False)
